f13a868a0c88d54abf765609737a13b54f824a38
Simon <simon.whyte.lb@gmail.com>
2019-08-20 11:52:41 +0100

    Appveyor setup

--- ci/before_deploy.ps1
+++ ci/before_deploy.ps1
@@ -10,8 +10,40 @@ Set-Location $STAGE
 
 $ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).zip"
 
-# TODO Update this to package the right artifacts
-Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\hello.exe" '.\'
+# Copy Plugin items
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\wooting_analog_common.lib" '.\plugins\lib'
+
+## Copy c headers
+Copy-Item "$SRC_DIR\includes\plugin.h" '.\plugins\includes\'
+Copy-Item "$SRC_DIR\includes\wooting-analog-plugin-dev.h" '.\plugins\includes\'
+Copy-Item "$SRC_DIR\includes\wooting-analog-common.h" '.\plugins\includes\'
+
+## Copy cpp headers
+Copy-Item "$SRC_DIR\includes-cpp\wooting-analog-plugin-dev.h" '.\plugins\includes-cpp\'
+Copy-Item "$SRC_DIR\includes-cpp\wooting-analog-common.h" '.\plugins\includes-cpp\'
+
+## Copy docs
+Copy-Item "$SRC_DIR\PLUGINS.md" '.\plugins\'
+
+
+
+# Copy wrapper items
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\wooting_analog_wrapper.dll" '.\wrapper\'
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\wooting_analog_sdk.dll" '.\wrapper\sdk\'
+
+## Copy c headers
+Copy-Item "$SRC_DIR\includes\wooting-analog-wrapper.h" '.\wrapper\includes\'
+Copy-Item "$SRC_DIR\includes\wooting-analog-common.h" '.\wrapper\includes\'
+
+## Copy cpp headers
+Copy-Item "$SRC_DIR\includes-cpp\wooting-analog-wrapper.h" '.\wrapper\includes-cpp\'
+Copy-Item "$SRC_DIR\includes-cpp\wooting-analog-common.h" '.\wrapper\includes-cpp\'
+
+## Copy docs
+Copy-Item "$SRC_DIR\SDK_USAGE.md" '.\wrapper\'
+
+# Copy README
+#Copy-Item "$SRC_DIR\README.md" '.\wrapper\'
 
 7z a "$ZIP" *
 
