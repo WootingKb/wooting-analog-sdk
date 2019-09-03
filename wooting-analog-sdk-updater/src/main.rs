f08c13c3588799c59ff55bff66fe2c974df37d64
Simon Whyte <simon.whyte.lb@gmail.com>
2019-09-03 14:40:01 +0100

    Updated updater to output json of update check result

--- wooting-analog-sdk-updater/src/main.rs
+++ wooting-analog-sdk-updater/src/main.rs
@@ -4,6 +4,8 @@ extern crate self_update;
 extern crate log;
 extern crate env_logger;
 extern crate winapi;
+#[macro_use]
+extern crate json;
 
 use std::ptr::null_mut as NULL;
 #[cfg(windows)]
@@ -25,7 +27,7 @@ fn find_installer_asset(release: &Release) -> Option<&ReleaseAsset> {
         .find(|asset| asset.name.ends_with(".msi"))
 }
 
-fn check_for_update() -> Result<Option<Release>, Box<dyn ::std::error::Error>> {
+fn check_for_update() -> Result<Release, Box<dyn ::std::error::Error>> {
     let releases = self_update::backends::github::ReleaseList::configure()
         .repo_owner("WootingKb")
         .repo_name("wooting-analog-sdk")
@@ -35,31 +37,10 @@ fn check_for_update() -> Result<Option<Release>, Box<dyn ::std::error::Error>> {
     info!("We found {:?}", releases);
     if !releases.is_empty() {
         let latest = releases.first().unwrap();
-        let release_ver = latest.tag.trim_start_matches('v');
-        debug!("Latest Github release has got version: {}", release_ver);
-        if bump_is_greater(PKG_VER, release_ver).unwrap_or(false) {
-            debug!(
-                "Github release {} is greater than our {} so let's update",
-                release_ver,
-                PKG_VER
-            );
-            if find_installer_asset(latest).is_some() {
-                Ok(Some(latest.clone()))
-            } else {
-                error!("We found latest release with ver {}, but we can't find an msi installer for it", release_ver);
-                Ok(None)
-            }
-        } else {
-            debug!(
-                "Github release {} is not greater than our {} so let's chill",
-                release_ver,
-                PKG_VER
-            );
-            Ok(None)
-        }
+        Ok(latest.clone())
     } else {
         warn!("No releases found on github");
-        Ok(None)
+        Err(From::from("Couldn't find any releases on github"))
     }
 }
 
@@ -118,36 +99,51 @@ fn main() {
                 .index(1),
         )*/
         .get_matches();
-    env_logger::from_env(Env::default().default_filter_or("wooting_analog_sdk_updater=trace")).init();
+    env_logger::init();
+    //env_logger::from_env(Env::default().default_filter_or("wooting_analog_sdk_updater=trace")).init();
 
-    let release = check_for_update().expect("Failed to check for updates");
-    match release {
-        Some(r) => {
-            info!("New version found: {}", r.version());
-            if !matches.is_present("no_install") {
-                #[cfg(windows)]
-                {
-                    if !matches.is_present("quiet") {
-                        let title = "Wooting Analog SDK Update\0";
-                        let message = format!("A new Wooting Analog SDK update is available ({}, you've got v{}), would you like to install?\0", r.tag, PKG_VER);
-                        let l_msg: Vec<u16> = message.encode_utf16().collect();
-                        let l_title: Vec<u16> = title.encode_utf16().collect();
-                        unsafe {
-                            if winuser::MessageBoxW(NULL(), l_msg.as_ptr(), l_title.as_ptr(), winuser::MB_YESNO | winuser::MB_ICONQUESTION) != winuser::IDYES {
-                                info!("User did not want update, closing");
-                                return;
-                            }
-                        }
+    let r = check_for_update().expect("Failed to check for updates");
+    let release_ver = r.tag.trim_start_matches('v');
+    let update_available = bump_is_greater(PKG_VER, release_ver).unwrap_or(false);
+    debug!(
+        "Github release: {} ours: {}, update available: {}",
+        release_ver,
+        PKG_VER,
+        update_available
+    );
+
+    if !matches.is_present("no_install") && update_available {
+        #[cfg(windows)]
+        {
+            if !matches.is_present("quiet") {
+                let title = "Wooting Analog SDK Update\0";
+                let message = format!("A new Wooting Analog SDK update is available ({}, you've got v{}), would you like to install?\0", r.tag, PKG_VER);
+                let l_msg: Vec<u16> = message.encode_utf16().collect();
+                let l_title: Vec<u16> = title.encode_utf16().collect();
+                unsafe {
+                    if winuser::MessageBoxW(NULL(), l_msg.as_ptr(), l_title.as_ptr(), winuser::MB_YESNO | winuser::MB_ICONQUESTION) != winuser::IDYES {
+                        info!("User did not want update, closing");
+                        return;
                     }
                 }
-
-                install_update(r).expect("Failed to install updates");
             }
         }
-        None => {
-            info!("You are using the latest version");
-        }
+
+        debug!("Attempting to update");
+        install_update(r).expect("Failed to install updates");
     }
+    else {
+        let data = object!{
+            "name" => "Wooting Analog SDK",
+            "update_available" => update_available,
+            "new_version"    => r.version(),
+            "version"     => PKG_VER,
+            "release_title" => r.name,
+            "release_notes" => r.body
+        };
+        println!("{}", data.dump());
+    }
+
 
     //Install this process as a scheduled task that runs every so often
 }
