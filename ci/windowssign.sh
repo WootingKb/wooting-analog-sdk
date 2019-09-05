c636998d80805d472751bdb3f6c0e34b60f3fbb1
Jeroen Langelaan <jeroen@wooting.nl>
2019-09-05 22:13:18 +0800

    Try windows code sign

---
+++ ci/windowssign.sh
@@ -0,0 +1,17 @@
+if [ $TRAVIS_OS_NAME = windows ]; then
+  set -e
+
+  export PATH="C:\Program Files (x86)\Windows Kits\10\bin\x64":$PATH
+  export BINARY_FILE=$1
+
+  # choco install -y windows-sdk-10.0
+
+  curl -v "$WIN_CSC_LINK" --output cert.pfx.gpg
+  gpg --passphrase ${WIN_CSC_KEY_PASSWORD} --batch -o cert.pfx -d cert.pfx.gpg
+
+  powershell Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
+  powershell Get-ExecutionPolicy -List
+
+  powershell $PWD/.build/codesign.ps1
+  signtool.exe verify -pa "$BINARY_FILE"
+fi
