6afbfa723b330dbce81a247c1f508de149914aca
Jeroen Langelaan <jeroen@wooting.nl>
2019-09-06 12:28:20 +0800

    try remove debug parameter

--- ci/codesign.ps1
+++ ci/codesign.ps1
@@ -4,4 +4,4 @@ dir cert:/LocalMachine
 
 $Password = ConvertTo-SecureString -String $Env:WIN_CSC_KEY_PASSWORD -AsPlainText -Force
 Import-PfxCertificate -FilePath cert.pfx -CertStoreLocation Cert:\LocalMachine\My -Password $Password
-Start-Process -PassThru -Wait signtool.exe -ArgumentList "sign -v -debug -sm -s My -n `"$Env:CERT_SUBJECTNAME`" -d `"$Env:CODESIGN_DESC`" `"$Env:BINARY_FILE`""
+Start-Process -PassThru -Wait signtool.exe -ArgumentList "sign -v -sm -s My -n `"$Env:CERT_SUBJECTNAME`" -d `"$Env:CODESIGN_DESC`" `"$Env:BINARY_FILE`""
