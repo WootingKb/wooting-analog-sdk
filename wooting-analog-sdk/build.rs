91bb5399d4607bec63d0028b6fc1dc3239fde691
simon-wh <simon.whyte.lb@gmail.com>
2020-01-27 17:23:55 +0000

    Update analog plugin and hopefully fully CI building of test c plugin

--- wooting-analog-sdk/build.rs
+++ wooting-analog-sdk/build.rs
@@ -2,6 +2,8 @@ const TEST_PLUGIN_DIR: &str = "test_c_plugin";
 
 fn main() {
     println!("Building Test C Plugin");
+    println!("{:?}", std::env::var("OUT_DIR"));
+
     cmake::Config::new(TEST_PLUGIN_DIR)
         .no_build_target(true)
         .out_dir(format!("./{}", TEST_PLUGIN_DIR))
