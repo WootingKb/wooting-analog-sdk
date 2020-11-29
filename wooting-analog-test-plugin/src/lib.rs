e695f9f4fa313a470dd26a1fc6c532ea3570144a
simon-wh <simon.whyte.lb@gmail.com>
2020-11-29 19:04:32 +0000

    Disable bs test

--- wooting-analog-test-plugin/src/lib.rs
+++ wooting-analog-test-plugin/src/lib.rs
@@ -307,8 +307,8 @@ declare_plugin!(WootingAnalogTestPlugin, WootingAnalogTestPlugin::new);
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    // #[test]
+    // fn it_works() {
+    //     assert_eq!(2 + 2, 4);
+    // }
 }
