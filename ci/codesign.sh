7709ecbca0d06322b8002de48dcff50b35a0a865
simon-wh <simon@wooting.io>
2022-06-10 19:31:48 +0100

    Temp disable Intel Mac & Linux build to test arm macos & windows
    Fix syntax?
    
    
    le fix
    
    
    Fix codesign.sh

--- ci/codesign.sh
+++ ci/codesign.sh
@@ -1,5 +1,5 @@
 # Thanks https://github.com/electron-userland/electron-builder/issues/3629#issuecomment-473238513
-if [ $TRAVIS_OS_NAME = windows ]; then
+if [ $RUNNER_OS = Windows ]; then
   set -e
 
   export PATH="C:\Program Files (x86)\Windows Kits\10\bin\x64":$PATH
