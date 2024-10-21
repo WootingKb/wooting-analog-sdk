97f9885d48abe27fb4c817fa9e683e581c5c3a5d
simon-wh <simon@wooting.io>
2024-10-21 18:01:12 +0200

    Re-enable test plugin building

--- wooting-analog-sdk/build.rs
+++ wooting-analog-sdk/build.rs
@@ -1,13 +1,13 @@
 const TEST_PLUGIN_DIR: &str = "test_c_plugin";
 
 fn main() {
-    // println!("Building Test C Plugin");
-    // println!("{:?}", std::env::var("OUT_DIR"));
+    println!("Building Test C Plugin");
+    println!("{:?}", std::env::var("OUT_DIR"));
 
-    // cmake::Config::new(TEST_PLUGIN_DIR)
-    //     .no_build_target(true)
-    //     .out_dir(format!("./{}", TEST_PLUGIN_DIR))
-    //     .always_configure(false)
-    //     .profile("Debug")
-    //     .build();
+    cmake::Config::new(TEST_PLUGIN_DIR)
+        .no_build_target(true)
+        .out_dir(format!("./{}", TEST_PLUGIN_DIR))
+        .always_configure(false)
+        .profile("Debug")
+        .build();
 }
